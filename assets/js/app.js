b633e1b1c25c562b9aabcf4a23a0c2cd7f703826
Josh Carroll <Josh Carroll>
2016-06-11 12:55:29 -0500

    track ids sorted into genre arrays

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+var genreObj = {};
 
 $('#lyric-btn').on('click', function (){
 
@@ -28,7 +28,8 @@ $('#lyric-btn').on('click', function (){
 	    	console.log(response);
 	    	$('#genreButtons').empty();
 
-	    	var genreArray = [];
+	    	// var genreArray = [];
+	    	//var genreObj = {};
 
 	    	for (var i=0; i<response.message.body.track_list.length; i++) {
 
@@ -38,23 +39,36 @@ $('#lyric-btn').on('click', function (){
 	    		var genreBool = true;
 	    		
 
-		    		for (var j=0; j < genreArray.length; j++) {
-		    			if (genreArray[j] === genreName) {
-		    				genreBool = false;
-		    			}
-		    		}
+		    		// for (var j=0; j < genreArray.length; j++) {
+		    		// 	if (genreArray[j] === genreName) {
+		    		// 		genreBool = false;
+		    		// 	}
+		    		// }
+		    		for (var prop in genreObj){
+		    			if (genreObj.hasOwnProperty(prop)){
+		    				if(prop === genreName){
+		    					genreBool = false;
+		    					genreObj[prop].push(response.message.body.track_list[i].track.track_id);
+		    				}
 
+		    			}
+
+		    			
+		    		}
 		    		// console.log(response[i]);
 		    		// console.log(genreName);
 
-		    		if (genreBool) {
-			    		var genre = $('<button class="genre">' + genreName + '</button>');
-			    		$('#genreButtons').append(genre);
-			    		genreArray.push(genreName);
-		    		}
+		    		 if (genreBool) {
+			    	 	var genre = $('<button class="genre">' + genreName + '</button>');
+			    	 	$('#genreButtons').append(genre);
+			    	 	//genreArray.push(genreName);
+			    	 	genreObj[genreName] = [response.message.body.track_list[i].track.track_id];
+			    	 	
+		    		 }
 	    		
 	    		};
 	    	} 
+	    	console.log(genreObj);
 	    	
 	    };
 
