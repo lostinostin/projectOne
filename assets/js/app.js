097b11340601e166daa876b574dd69491a342e5b
Chris Crawford <chrisccrawford@utexas.edu>
2016-06-13 00:34:37 -0500

    grabbed track id's from clicked genre button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,21 @@
 $(document).ready(function () {
 
-var genreObj = {};
+	var genreObj = {};
+	var queryURL;
+	var genreyName;
+	var track;
+	var genreBool;
+	var genre;
+	var userClick;
 
+	$('#lyric-btn').on('click', function (){
 
-$('#lyric-btn').on('click', function (){
+		userInput = $('#lyric-input').val().trim();
+		$('#lyric-input').val("");
+		queryURL = "http://api.musixmatch.com/ws/1.1/track.search";
 
-	userInput = $('#lyric-input').val().trim();
-	$('#lyric-input').val("");
-	var queryURL = "http://api.musixmatch.com/ws/1.1/track.search";
+		console.log(queryURL);
 
-	console.log(queryURL);
-
-	//$('#gifsView').empty();
 	    $.ajax({
 	        url: queryURL,
 
@@ -22,66 +26,106 @@ $('#lyric-btn').on('click', function (){
 		        format: "jsonp",
 		        callback: 'jsonpCallback',
 		        apikey: '455f0aefff3b00f8f559b6b271f6a28d',
-		        q_track: userInput
+		        // Changed from q_track to q_lyrics
+		        q_lyrics: userInput
 		    }
 	    });
 
 	    window.jsonpCallback = function(response) {
+
 	    	console.log(response);
 	    	$('#genreButtons').empty();
-
-
+	    	
 
 	    	for (var i=0; i<response.message.body.track_list.length; i++) {
 
 	    		if (response.message.body.track_list[i].track.primary_genres.music_genre_list.length > 0) {
 
-	    		var genreName = response.message.body.track_list[i].track.primary_genres.music_genre_list[0].music_genre.music_genre_name;
-	    		var genreBool = true;
-	    		
+		    		genreName = response.message.body.track_list[i].track.primary_genres.music_genre_list[0].music_genre.music_genre_name;
+	    			track = response.message.body.track_list[i].track.track_id
+	    		 	genreBool = true;
 
-		    		// for (var j=0; j < genreArray.length; j++) {
-		    		// 	if (genreArray[j] === genreName) {
-		    		// 		genreBool = false;
-		    		// 	}
-		    		// }
+	    			// Iterates over the properties of the object
+	    			// This won't run if the property isn't in the object
 		    		for (var prop in genreObj){
-		    			if (genreObj.hasOwnProperty(prop)){
-		    				if(prop === genreName){
+		    			// Initial condition to begin checking if a property is in genreObj
+		    			if (genreObj.hasOwnProperty(prop)) {
+		    				// If a property exists in the object that is equal to genreName 
+		    				if (prop === genreName) {
 		    					genreBool = false;
-		    					genreObj[prop].push(response.message.body.track_list[i].track.track_id);
+		    					genreObj[prop].push(track);
 		    				}
-
 		    			}
-
-		    			
 		    		}
+
 		    		// console.log(response[i]);
 		    		// console.log(genreName);
 
-		    		 if (genreBool) {
-			    	 	var genre = $('<button class="genre">' + genreName + '</button>');
+		    		if (genreBool) {
+			    	 	genre = $('<button id="'+ genreName + '" class="genre">' + genreName + '</button>');
 			    	 	$('#genreButtons').append(genre);
-			    	 	//genreArray.push(genreName);
-			    	 	genreObj[genreName] = [response.message.body.track_list[i].track.track_id];
-			    	 	
-		    		 }
+			    	 	genreObj[genreName] = [track];
+
+		    		}
 	    		
 	    		};
-	    	} 
-	    	console.log(genreObj);
-	    	
+	    	} 	    	
 	    };
+	});
 
+	$('#genreButtons').on('click', 'button', function(e){
+		var genreClicked = e.target.id;
+		console.log(genreClicked);
+		console.log(genreObj);
 
+		/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
-});
-
-$('#genreButtons').on('click', function(){
-
-});
-
-});
-
+		You can loop through the object properties like Josh did. You can also do it with the 
+		jQuery.each() funciton as seen below. Either one works :D
 	
+		// for (var prop in genreObj) {
+		// 	if (genreClicked === prop) {
+		// 		alert(genreObj[prop]);
+		// 	}
+		// }
 
+		-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
+
+		$.each(genreObj, function(key, element) {
+
+			if (genreClicked === key) {
+				alert([element])
+			}
+	 		//alert('key: ' + key + '\n' + 'value: ' + [element]);
+		});
+	});
+
+	// 	queryURL = "http://api.musixmatch.com/ws/1.1/track.get";
+	// 	userClick = $(genreObj.attr('id');
+	// 	console.log(e.genreObj);
+	// 	console.log(userClick);
+
+	//     $.ajax({
+	//         url: queryURL,
+
+	//         method: "GET",
+
+	//         dataType: 'jsonp',
+	//         data: {
+	// 	        format: "jsonp",
+	// 	        callback: 'jsonpCallback',
+	// 	        apikey: '455f0aefff3b00f8f559b6b271f6a28d',
+	// 	        track_id: userClick
+	// 	    }
+	//     });
+
+	//     window.jsonpCallback = function(response) {
+	//     	console.log(response);
+
+
+	//     };
+
+
+	// });
+
+});
