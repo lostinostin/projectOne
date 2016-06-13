3a3182d4b64bb802a8d33c110e12b4499eeda759
Chris Crawford <chrisccrawford@utexas.edu>
2016-06-13 01:23:34 -0500

    running into a scoping issue with trackIdArray

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,9 @@ $(document).ready(function () {
 	var genreBool;
 	var genre;
 	var userClick;
+	var genreClicked;
+	var j;
+	var trackIdArray = [];
 
 	$('#lyric-btn').on('click', function (){
 
@@ -74,7 +77,7 @@ $(document).ready(function () {
 	});
 
 	$('#genreButtons').on('click', 'button', function(e){
-		var genreClicked = e.target.id;
+		genreClicked = e.target.id;
 		console.log(genreClicked);
 		console.log(genreObj);
 
@@ -94,11 +97,20 @@ $(document).ready(function () {
 		$.each(genreObj, function(key, element) {
 
 			if (genreClicked === key) {
-				alert([element])
-			}
-	 		//alert('key: ' + key + '\n' + 'value: ' + [element]);
-		});
-	});
+				for (j=0; j<element.length; j++) {
+
+					trackIdArray.push(element[j]);
+
+				}
+				
+			} 
+		
+		}); console.log(trackIdArray);
+		
+		
+	}); 
+
+	
 
 	// 	queryURL = "http://api.musixmatch.com/ws/1.1/track.get";
 	// 	userClick = $(genreObj.attr('id');
